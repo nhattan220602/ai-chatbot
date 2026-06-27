1f80de502f33eb2b61d93830099718a2f1add4a6
Nhat Tan <nhattan220602@gmail.com>
2026-06-27 21:26:13 +0700

    feat: add the user's sent message to the chat list and create new message

--- src/script.js
+++ src/script.js
@@ -1,10 +1,30 @@
 const messageInput = document.querySelector(".message-input");
+const chatBody = document.querySelector(".chat-body");
+
+
+
+//Create message element with dynamic classes and return it
+const createMessageElement = (content, classes) => {
+    const div = document.createElement("div");
+    div.classList.add("message", classes);
+    div.innerHTML = content;
+    return div;
+}
+
+//Handle outgoing user messages
+const HandelOutgoingMessage = (userMessage) => {
+    
+    //Create and display user message
+    const messageContent = `<div class="message-text">${userMessage}</div>`;
+    const outGoingMessageDiv = createMessageElement(messageContent, "user-message");
+    chatBody.appendChild(outGoingMessageDiv);
+};
 
 //Handle Enter key press for sending message
 messageInput.addEventListener("keydown", (e) => {
     const userMessage = e.target.value.trim();
 
     if (e.key === "Enter" && userMessage) {
-        console.log(userMessage);
+        HandelOutgoingMessage(userMessage);
     }
 });
