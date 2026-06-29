029e1b7dbc31aa72fd1ddeb22252d3b387b41b58
Nhat Tan <nhattan220602@gmail.com>
2026-06-30 01:05:30 +0700

    feat: add file preview with delete functionality in chat form

--- src/script.js
+++ src/script.js
@@ -2,6 +2,9 @@ const messageInput = document.querySelector(".message-input");
 const chatBody = document.querySelector(".chat-body");
 const sendMessageButton = document.querySelector("#send-message");
 const fileInput = document.querySelector("#file-input");
+const fileUploadWrapper = document.querySelector(".file-upload-wrapper");
+const fileCancelButton = document.querySelector("#file-cancel");
+
 
 //API setup
 const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${API_KEY}`;
@@ -82,11 +85,16 @@ const handelOutgoingMessage = (e) => {
     userData.message = messageInput.value.trim();
 
     messageInput.value = "";
+    fileUploadWrapper.classList.remove("file-uploaded");
+
 
     //Create and display user message
     const messageContent = `<div class="message-text"></div>
-                            ${userData.file.data ? `<img src = "data:${userData.file.mime_type};base64, 
-                            ${userData.file.data}" class="attachment" />` : ""} `;
+                            ${userData.file.data
+            ? `<img src = "data:${userData.file.mime_type};base64, 
+                            ${userData.file.data}" class="attachment" />`
+            : ""
+        } `;
     const outGoingMessageDiv = createMessageElement(
         messageContent,
         "user-message",
@@ -159,6 +167,10 @@ fileInput.addEventListener("change", () => {
         const reader = new FileReader();
 
         reader.onload = (e) => {
+            //Preview the selected file
+            fileUploadWrapper.querySelector("img").src = e.target.result;
+            fileUploadWrapper.classList.add("file-uploaded");
+
             const base64String = e.target.result.split(",")[1];
 
             //Store file data in userData
@@ -184,3 +196,9 @@ sendMessageButton.addEventListener("click", (e) => {
 document
     .querySelector("#file-upload")
     .addEventListener("click", () => fileInput.click());
+
+//File cancel button
+fileCancelButton.addEventListener("click", (e) => {
+    userData.file = {};
+    fileUploadWrapper.classList.remove("file-uploaded")
+});
